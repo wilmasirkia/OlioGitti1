874e6aca7ea810b086792a1c3f9f39c41a5ce3fa
wilmasirkia <wilma20104@student.hamk.fi>
2021-12-07 00:08:11 +0200

    Initial commit

---
+++ src/Lelu.java
@@ -0,0 +1,57 @@
+public class LeluTehtava {
+
+	public static void main(String[] args) {
+		Lelu lelu = new Lelu("Toyota", 5);
+		lelu.tulostaTiedot();
+		
+		VieteriAuto vieteriAuto = new VieteriAuto("Renault", 10, 100);
+		vieteriAuto.tulostaTiedot();
+		}		
+} //t‰h‰n p‰‰ttyy
+
+class Lelu {
+	
+	public String nimi; 
+	public int paino;
+
+	public Lelu() {
+		nimi="";
+		paino=0;
+	}
+
+	public Lelu(String nimi, int paino) {
+		this.nimi = nimi;
+		this.paino = paino; 
+	}
+	
+	public void tulostaTiedot() {
+		System.out.println("Nimi: " + nimi);
+		System.out.println("Paino: " + paino + "kg");
+	}
+} 
+
+class VieteriAuto extends Lelu {
+	public int nopeus;
+	public VieteriAuto() {
+		paino = 0; 
+		nimi = ""; 
+		nopeus = 0; 
+	}
+	
+	public VieteriAuto(String nimi, int paino, int nopeus) {
+		this.nimi = nimi;
+		this.paino = paino;
+		this.nopeus = nopeus;
+	}
+	
+	public void kiihdyta(int nopeus) {
+		this.nopeus = nopeus;
+		nopeus = nopeus++;
+	}
+	
+	@Override  
+	public void tulostaTiedot()	{    
+    super.tulostaTiedot();
+		System.out.println("Nopeus: " + nopeus + "km/h");
+	}	
+}
